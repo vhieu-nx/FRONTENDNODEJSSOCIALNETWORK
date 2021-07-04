a7abec774c1015e591b858c8db259ff7e1461162
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-04 13:34:29 +0700

    front end

--- src/components/auth/StartPage.js
+++ src/components/auth/StartPage.js
@@ -14,7 +14,7 @@ const StartPage = () => {
             <div>
                 <div className="container text-center start-page-margin" >
                     <div className="jumbotron bg-light text-dark text-center mb-0 mt-5 mx-auto jumbo-wrapper" style={{ 'boxShadow': '0 0 14px 1px rgba(0, 0, 0, 0.3)' }}>
-                        <h2 className="h1 h1-responsive">Welcome to SoftUni Social Network!</h2>
+                        <h2 className="h1 h1-responsive">Welcome to Social Network!</h2>
                         <div className="hr-styles" style={{'width': '70%'}}></div>
                         <p className="lead" style={{'marginBottom': '1.5rem', 'fontWeight': '600'}}>Please <NavLink className="text-info" exact to="/login">Login</NavLink> or <NavLink className="text-info" exact to="/register">Register</NavLink> if you don't have an account.</p>
                         <div className="hr-styles" style={{'width': '70%'}}></div>
@@ -33,7 +33,7 @@ const StartPage = () => {
                     <div className="jumbotron bg-light text-dark text-center mb-0 mt-5 jumbo-wrapper" style={{ 'boxShadow': '0 0 14px 1px rgba(0, 0, 0, 0.3)' }}>
                         <h3 className="md-display-5 h3 h3-responsive mb-3">Hello {userService.getUsername()}!</h3>
                         <div className="hr-styles" style={{'width': '80%'}}></div>
-                        <h2 className="h1 h1-responsive">Welcome to SoftUni Social Network!</h2>
+                        <h2 className="h1 h1-responsive">Welcome to  Social Network!</h2>
                         <div className="hr-styles" style={{'width': '80%'}}></div>
                         <p className="lead">
                             <NavLink className="btn App-button-primary btn-lg m-3" to={`/home/comments/${currentUserId}`} role="button">Home</NavLink>
--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
     <footer className="App-footer">
         <div className="mb-5">
-            <p className="footer"> &copy; Social Network @ KL 2019. All rights reserved.</p>
+            <p className="footer"> &copy; HIEUTHANTHIEN @ KH 2019</p>
         </div>
     </footer>
 );
