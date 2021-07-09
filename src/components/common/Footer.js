89a79ea87714bcad763ffb1dda32c3646ae8a147
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-09 09:32:54 +0700

    front end social network use nodejs

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
     <footer className="App-footer">
         <div className="mb-5">
-            <p className="footer"> &copy; HIEUTHANTHIEN @ KH 2019</p>
+            <p className="footer"> &copy; HIEUTHANTHIEN</p>
         </div>
     </footer>
 );
